f95bc7f9ff91d497999bcc249bcc9925a497e340
noealas53 <00262020@uca.edu.sv>
2024-07-17 23:07:20 -0600

    Add dependency on Google Maps and show points on the map and in the customer table

--- src/pages/ClientMap/index.jsx
+++ src/pages/ClientMap/index.jsx
@@ -1,9 +1,53 @@
-const ClientMap = () => {
-    return (
-        <div>
-            <h1>ClientMap</h1>
-        </div>
-    );
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api'
+import React from 'react'
+import { points } from '../../data/points'
+
+const center = {
+    north: 14.4501,
+    south: 13.1483,
+    west: -90.1289,
+    east: -87.6886 
 }
 
-export default ClientMap
+const ClientMap = () => {
+
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: 'AIzaSyBjyM0CyjiksJbMk4SVzZTz-Uzn5QusoRE'
+    })
+
+    const [map, setMap] = React.useState(null)
+
+    const onLoad = React.useCallback(function callback(map) {
+        const bounds = new window.google.maps.LatLngBounds(center);
+        map.fitBounds(bounds);
+
+        setMap(map)
+    }, [])
+
+    const onUnmount = React.useCallback(function callback(map) {
+        setMap(null)
+    }, [])
+
+    return isLoaded ? (
+        <GoogleMap
+            mapContainerClassName='w-full h-full'
+            center={center}
+            onLoad={onLoad}
+            zoom={10}
+            onUnmount={onUnmount}
+        >
+            {
+                points.map((point, index) => (
+                    <Marker
+                        key={index}
+                        position={{ lat: point.lat, lng: point.lng }}
+                    />
+                ))
+            }
+            <></>
+        </GoogleMap>
+    ) : <></>
+}
+
+export default React.memo(ClientMap)
--- src/pages/ClientTable/index.jsx
+++ src/pages/ClientTable/index.jsx
@@ -1,3 +1,4 @@
+import { points } from '../../data/points'
 
 const ClientTable = () => {
     return (
@@ -11,31 +12,27 @@ const ClientTable = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td className='p-3'>Punto 1</td>
-                        <td className='p-3'>13.705243</td>
-                        <td className='p-3'>-89.24231</td>
-                    </tr>
-                    <tr className=' bg-gray-200'>
-                        <td className='p-3'>Punto 2</td>
-                        <td className='p-3'>13.696674</td>
-                        <td className='p-3'>-89.197927</td>
-                    </tr>
-                    <tr>
-                        <td className='p-3'>Punto 3</td>
-                        <td className='p-3'>14.692511</td>
-                        <td className='p-3'>-87.86136</td>
-                    </tr>
-                    <tr className=' bg-gray-200'>
-                        <td className='p-3'>Punto 4</td>
-                        <td className='p-3'>12.022747</td>
-                        <td className='p-3'>-86.252007</td>
-                    </tr>
-                    <tr>
-                        <td className='p-3'>Punto 5</td>
-                        <td className='p-3'>8.103289</td>
-                        <td className='p-3'>-80.596013 </td>
-                    </tr>
+                    {
+                        points.map((point, index) => {
+                            if (index % 2 === 0) {
+                                return (
+                                    <tr key={index} className='text-center bg-gray-100'>
+                                        <td className='p-3'>{point.name}</td>
+                                        <td className='p-3'>{point.lat}</td>
+                                        <td className='p-3'>{point.lng}</td>
+                                    </tr>
+                                )
+                            } else {
+                                return (
+                                    <tr key={index} className='text-center'>
+                                        <td className='p-3'>{point.name}</td>
+                                        <td className='p-3'>{point.lat}</td>
+                                        <td className='p-3'>{point.lng}</td>
+                                    </tr>
+                                )
+                            }
+                        })
+                    }
                 </tbody>
             </table>
         </div>
